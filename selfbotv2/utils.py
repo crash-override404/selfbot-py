d8307d5cf7766feb681ffeb4cc7b13af03949070
{Zero Cool} <ancha.roca002@gmail.com>
2020-03-24 12:35:18 +0800

    Add selfbotv2 (implement your self)

---
+++ selfbotv2/utils.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class HelpOptions:
+    Prefix = '╭───「 Help 」'
+    Header = '├──「 %s 」'
+    Footer = '╰───「 Hello World 」'
+    Left = '├≽ '
+    Num = ''
+    Doc = True
+    DocSep = '\n│ • '
+    ArgsSep = '/'
+    ArgsLeft = '<'
+    ArgsRight = '>'
